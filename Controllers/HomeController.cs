f8ea75402736907d099e7833f949eed1b57e48ca
Rey <reudysfernando@gmail.com>
2025-12-19 18:43:32 -0400

    Boton Incrementar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+
+// Acceso a todos los modelos
 using Proyecto.Models;
 
 namespace Proyecto.Controllers
@@ -16,6 +18,16 @@ namespace Proyecto.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        // Funcion que incrementa en uno
+        public IActionResult SumarUno()
+        {
+            // Llamamos al modelo y luego al atributo. Como Apellido y nombre
+            num.numero++;
+
+            // Regresa a Index al pulsar el boton
+            return Redirect("Index");
         }
 
         public IActionResult Privacy()
---
+++ Models/num.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto.Models
+{
+    public class num
+    {
+        // Un atrivuto basico 
+        public static int numero = 0;
+        
+        // Como dato, si escribes prop y le das enter te aparece una propiedad por default
+    }
+}
